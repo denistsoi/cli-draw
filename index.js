ac3fbc558d690973081a39795504ff42192bbb4d
Denis Tsoi <denistsoi@gmail.com>
2018-01-17 17:16:55 +0800

    ADD: format project structure;

--- index.js
+++ index.js
@@ -5,51 +5,15 @@
  */
 
 /**
- * import dependencies
+ * import external dependencies
  */
-
-
 
 /**
- * define commands
+ * import helper commands
  */
 
-
-/**
- * createCanvas
- * @description create an emptyCanvas based on width, height
- * @param {Number} width canvas width
- * @param {Number} height canvas height
- * 
- * @return {Array} 
- */
-const createCanvas = function(width, height) {
-  // define rows "height" of canvas
-  let canvas = new Array(height);
-
-  // define cells in row based on width
-  for (let i = 0; i < canvas.length; i++) {
-    canvas[i] = new Array(width);
-  }
-  
-  return canvas;
-}
-
-/**
- * drawLine
- * @description draw a straight line
- */
-const drawLine = function(x1, y1, x2, y2) {
-
-}
-
-/**
- * draw Rectangle
- */
-const drawRectangle = function(x1, y1, x2, y2) {
-  // use drawLine 
-}
-
-const fill = function(x, y, colour) {
-
-}
+const bucket = require('./lib/commands/bucket');
+const canvas = require('./lib/commands/canvas');
+const line = require('./lib/commands/line');
+const quit = require('./lib/commands/quit');
+const rectangle = require('./lib/commands/rectangle');
