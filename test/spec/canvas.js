246b7329407013c696a2c1f82f2fb8a4adbc8248
Denis Tsoi <denistsoi@gmail.com>
2018-01-21 01:36:09 +0800

    ADD: canvas, update tests;

--- test/spec/canvas.js
+++ test/spec/canvas.js
@@ -1,45 +1,43 @@
-const createCanvas = require('../../lib/commands/canvas');
-const canvasToString = require('../../lib/helpers/canvasToString');
+const Canvas = require('../../lib/commands/canvas');
+// const canvasToString = require('../../lib/helpers/canvasToString');
+// const createBorder = require('../../lib/helpers/createBorder');
 
 const expect = require('expect');
 const should = require('chai').should();
 
-describe('createCanvas', () => {
+describe('Canvas', () => {
   it('should create an internal canvas with width 4, height 5', () => {
 
-    let c = createCanvas(4,5);
-
-    expect(c[0].length).toEqual(4)
-    expect(c.length).toEqual(5)
-  });
+    let canvas = new Canvas(4,5);
+    expect(canvas.width).toEqual(4);
+    expect(canvas.height).toEqual(5);
+  })
 
   it('should create an internal canvas with width 20, height 4', () => {
-    let c = createCanvas(20,4);
-    expect(c[0].length).toEqual(20);
-    expect(c.length).toEqual(4);
-    // console.log(c);
+    
+    let canvas = new Canvas(20,4);
+    
+    expect(canvas.width).toEqual(20);
+    expect(canvas.height).toEqual(4);
   });
 
   it('should throw if params are missing', () => {
     (() => {
-      createCanvas()
+      let canvas = new Canvas();
     }).should.throw('width is undefined');
 
     (() => {
-      createCanvas(4)
+      let canvas = new Canvas(4);
     }).should.throw('height is undefined');
 
     // should not throw if params are defined
     (() => {
-      createCanvas(4,4)
+      let canvas = new Canvas(4,4);
     }).should.not.throw('width/height is undefined');
   });
-})
 
-describe('canvasToString', () => {
-  let c = createCanvas(4, 5);
-  // c = canvasToString(c);
-
-  console.log(c);
-
+  it('should return a canvas to string', () => {
+    let canvas = new Canvas(2,2);
+    canvas.printCanvas().should.equal('----\n'+'|  |\n'+'|  |\n'+'----\n');
+  })
 })
