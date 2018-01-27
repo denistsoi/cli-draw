e708c9f41408e65654363f2fcbc5ab36c1a9fc6e
Denis Tsoi <denistsoi@gmail.com>
2018-01-27 11:06:22 +0100

    ADD: line command;

--- test/spec/line.js
+++ test/spec/line.js
@@ -5,10 +5,155 @@ const expect = require('expect');
 const should = require('chai').should();
 
 describe('drawLine', () => {
-  it('should draw a line to a canvas', () => {
+
+  it('should draw a horizontal line to a canvas', () => {
+    let canvas = new Canvas(4,4);
+
+    drawLine(canvas, { x1: 2, y1: 1, x2: 3, y2: 1 })
+
+    canvas.printCanvas().should.equal([
+      '------',
+      '| xx |',
+      '|    |',
+      '|    |',
+      '|    |',
+      '------\n',
+    ].join('\n'));    
+  });
+
+  
+  it('should draw a horizontal line to a canvas', () => {
+    let canvas = new Canvas(4,4);
+
+    drawLine(canvas, { x1: 1, y1: 1, x2: 3, y2: 1 })
+
+    canvas.printCanvas().should.equal([
+      '------',
+      '|xxx |',
+      '|    |',
+      '|    |',
+      '|    |',
+      '------\n',
+    ].join('\n'));    
+  });
+
+  it('should draw a horizontal line from 4,1 to 5,1 on canvas', () => {
+    let canvas = new Canvas(5,4);
+
+    drawLine(canvas, { x1: 4, y1: 1, x2: 5, y2: 1 })
+
+    canvas.printCanvas().should.equal([
+      '-------',
+      '|   xx|',
+      '|     |',
+      '|     |',
+      '|     |',
+      '-------\n',
+    ].join('\n'));    
+  });
+
+  it('should draw a vertical line to a canvas', () => {
+    let canvas = new Canvas(4,4);
+
+    drawLine(canvas, { x1: 2, y1: 1, x2: 2, y2: 3})
+
+    canvas.printCanvas().should.equal([
+      '------',
+      '| x  |',
+      '| x  |',
+      '| x  |',
+      '|    |',
+      '------\n',
+    ].join('\n'));    
+  });
+
+  it('should draw a vertical line to a canvas', () => {
+    let canvas = new Canvas(4,4);
+
+    drawLine(canvas, { x1: 2, y1: 2, x2: 2, y2: 3})
+
+    canvas.printCanvas().should.equal([
+      '------',
+      '|    |',
+      '| x  |',
+      '| x  |',
+      '|    |',
+      '------\n',
+    ].join('\n'));    
+  });
+
+  it('should draw two lines line to a canvas', () => {
     let canvas = new Canvas(4,4);
 
     drawLine(canvas, { x1: 1, y1: 1, x2: 3, y2: 1})
-    console.log(canvas);
-  })
+    drawLine(canvas, { x1: 2, y1: 1, x2: 2, y2: 3})
+
+    canvas.printCanvas().should.equal([
+      '------',
+      '|xxx |',
+      '| x  |',
+      '| x  |',
+      '|    |',
+      '------\n',
+    ].join('\n'));
+  });
+
+
+  it('should draw two lines line to a canvas', () => {
+    let canvas = new Canvas(6,6);
+
+    drawLine(canvas, { x1: 2, y1: 2, x2: 2, y2: 4})
+
+    canvas.printCanvas().should.equal([
+      '--------',
+      '|      |',
+      '| x    |',
+      '| x    |',
+      '| x    |',
+      '|      |',
+      '|      |',
+      '--------\n',
+    ].join('\n'));
+
+    drawLine(canvas, { x1: 3, y1: 2, x2: 4, y2: 2})
+
+    canvas.printCanvas().should.equal([
+      '--------',
+      '|      |',
+      '| xxx  |',
+      '| x    |',
+      '| x    |',
+      '|      |',
+      '|      |',
+      '--------\n',
+    ].join('\n'));
+
+    drawLine(canvas, { x1: 4, y1: 2, x2: 4, y2: 4})
+
+    canvas.printCanvas().should.equal([
+      '--------',
+      '|      |',
+      '| xxx  |',
+      '| x x  |',
+      '| x x  |',
+      '|      |',
+      '|      |',
+      '--------\n',
+    ].join('\n'));
+
+    drawLine(canvas, { x1: 2, y1: 4, x2: 4, y2: 4})
+
+    canvas.printCanvas().should.equal([
+      '--------',
+      '|      |',
+      '| xxx  |',
+      '| x x  |',
+      '| xxx  |',
+      '|      |',
+      '|      |',
+      '--------\n',
+    ].join('\n'));    
+  });
+
+
 })
